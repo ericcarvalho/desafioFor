903287281fe87180bbff2f4b6bdf1ebafb29882a
Eric Carvalho <eric.dcvieira@gmail.com>
2026-06-22 23:14:19 -0300

    Usando argumentos, parametros, return

---
+++ scripts.js
@@ -0,0 +1,49 @@
+/*function sayMyName(valor1, valor2){
+    console.log(`total ${valor1 + valor2}`);
+}
+
+sayMyName(30, 10);
+*/
+
+
+
+
+const cart = [
+    { name: 'shirt', price: 20 },
+    { name: 'pants', price: 30 },
+    { name: 'shoes', price: 50 },
+    { name: 'hat', price: 15 },
+    { name: 'jacket', price: 80 },
+    { name: 'socks', price: 10 },
+    { name: 'belt', price: 25 },
+    { name: 'watch', price: 120 },
+    { name: 'scarf', price: 18 },
+    { name: 'gloves', price: 22 },
+    { name: 'sunglasses', price: 60 },
+    { name: 'backpack', price: 75 },
+    { name: 'wallet', price: 40 },
+
+];
+
+function totalPrice(cart) {
+    let totalPrice = 0;
+    for (let i = 0; i < cart.length; i++) {
+        totalPrice += cart[i].price;
+    }
+    return totalPrice;
+
+}
+
+function calculateTotal(cart) {
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].price > 30) {
+            total += cart[i].price * 0.9;
+        } else {
+            total += cart[i].price;
+        }
+    }
+    return total;
+}
+
+console.log(totalPrice(cart), `com desconto ${calculateTotal(cart)}`);
